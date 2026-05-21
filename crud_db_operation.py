8deba8d77c79acc4fe0b235750521e4c326bb746
iamngoc <168901457+iamngoc@users.noreply.github.com>
2026-05-21 17:58:40 +0200

    initial commit

---
+++ crud_db_operation.py
@@ -0,0 +1,195 @@
+"""
+C = create
+R = read
+U = update
+D = delete
+Write, read, update, delete datas in database
+"""
+
+from datetime import date, datetime, timedelta
+from typing import Optional, Type
+import logging
+import numpy as np
+from sqlalchemy.orm import Session
+from sqlalchemy import func, desc
+
+from database_model import Employee, TimeRecording
+
+logger = logging.getLogger(__name__)
+
+"""
+Employee operations (database "Employees")
+"""
+
+def fetch_all_employees(db: Session) -> list[Type[Employee]]:
+    """
+    fetch all active employees from database
+    :param db:
+    :return:
+    """
+    return (db.query(Employee)
+            .filter(Employee.active == True)
+            .all())
+
+def find_employee_from_id(db: Session, id: int) -> Optional[Employee]:
+    """
+    find one employee from database from given id
+    :param db:
+    :param id:
+    :return:
+    """
+    return (db.query(Employee)
+            .filter(Employee.id == id)
+            .first())
+
+def find_employee_from_email(db: Session, email: str) -> Optional[Employee]:
+    """
+    find one employee from given email
+    :param db:
+    :param email:
+    :return:
+    """
+    return (db.query(Employee).
+            filter(Employee.email == email)
+            .first())
+
+def fetch_embedding_all_employees(db: Session) -> list[dict]:
+    """
+    fetch all embeddings for facial recognition
+    :param db:
+    :return:
+    """
+    employees = fetch_all_employees(db)
+    results = []
+
+    for employee in employees:
+        if employee.embedding is not None:
+            results.append({"id": employee.id,
+                            "name": employee.name,
+                            "embedding": employee.embedding})
+
+    logger.debug(f"Embeddings loaded: {len(results)} employees")
+    return results
+
+def store_embedding(db: Session, employee_id: int, embedding: np.ndarray,
+                    photo_path: str = None) -> bool:
+    """
+    store facial fingerprint embedding of employee and will be called when enrollment
+    :param db:
+    :param employee_id:
+    :param embedding:
+    :param photo_path:
+    :return:
+    """
+    employee = find_employee_from_id(db, employee_id)
+    if not employee:
+        logger.error(f"employee {employee_id} not found")
+        return False
+
+    employee.embedding = embedding.tolist() # numpy -> python list
+    if photo_path:
+        employee.photo_path = photo_path
+    employee.updated_in = datetime.now()
+
+    db.commit()
+    logger.info(f"Embedding for {employee.name} saved.")
+    return True
+
+"""
+TimeRecording operations (database "TimeRecording")
+"""
+
+def determine_scan_typ(db: Session, employee_id: int) -> str:
+    """
+    determine whether the next scan type is COME or GO
+    if already COME then GO, otherwise COME
+    :param db:
+    :param employee_id:
+    :return: string
+    """
+    today = date.today()
+    # latest scan for today
+    latest_scan = (db.query(TimeRecording)
+                   .filter(TimeRecording.employee_id == employee_id,TimeRecording.scan_date==today)
+                   .order_by(TimeRecording.scan_time)
+                   .first())
+
+    if latest_scan is None:
+        return f"COME" # not scan yet
+    elif latest_scan.scan_type == "COME":
+        return f"GO" # already COME, now GO
+    else:
+        return f"COME" # already DO, now COME again
+
+def enter_scan (db: Session, employee_id: int, scan_type: str, confidence_score: float, camera_id: str ="entrance_00", snapshot_path = None) -> TimeRecording:
+    """
+    enter a new scan in the database TimeRecording
+    :param db: database session
+    :param employee_id: id of the employee
+    :param scan_type: come or go
+    :param confidence_score: check confidence score
+    :param camera_id: id of the camera
+    :param snapshot_path: path of the snapshot
+    :return: information of the new scan
+    """
+
+    now = datetime.now()
+    entry = TimeRecording(employee_id=employee_id,
+                          scan_time=now,
+                          scan_date=now.date(),
+                          scan_type=scan_type,
+                          confidence_score=confidence_score,
+                          camera_id=camera_id,
+                          snapshot_path=snapshot_path)
+
+    db.add(entry)
+    db.commit()
+    db.refresh(entry)
+
+    logger.info(f"TimeRecording entered: "
+                f"Employee: {employee_id} ,"
+                f"{scan_type}, {now.strftime('%Y-%m-%d %H:%M:%S')}, "
+                f"Score: {confidence_score} ")
+
+    return entry
+
+def today_report (db: Session) -> list[dict]:
+    """all scan for today as report"""
+    today = date.today()
+    scans = (
+        db.query(TimeRecording, Employee)
+        .join(Employee)
+        .filter(TimeRecording.scan_date == today)
+        .order_by(TimeRecording.scan_time)
+        .all()
+    )
+
+    return [
+        {"name": e.name,
+             "scan_type": t.scan_type,
+             "confidence_score": round(t.confidence_score, 3),
+             "camera_id": t.camera_id
+        }
+        for t, e in scans
+    ]
+
+def weekly_report (db: Session, employee_id: int) -> list[dict]:
+    """scan report of one employee in the last 7 days"""
+    seven_days_ago = datetime.today() - timedelta(days=7)
+    scans = (
+        db.query(TimeRecording)
+        .filter(
+            TimeRecording.employee_id == employee_id, TimeRecording.scan_date >= seven_days_ago)
+        .order_by(desc(TimeRecording.scan_date), TimeRecording.scan_time)
+        .all()
+    )
+
+    return [
+        {
+            "date": str(t.scan_date),
+            "scan_type": t.scan_type,
+            "time": t.scan_time.strftime("%Y-%m-%d %H:%M:%S"),
+            "score": t.confidence_score
+        }
+        for t in scans
+    ]
---
+++ enrollment_script.py
@@ -0,0 +1,176 @@
+"""
+This script register an employee with these steps:
+- open the camera (webcam)
+- take x photos of the employee (x = 10 or more depends on number you want)
+- compute the average embeddings
+- store the embeddings into the database 'employees'
+
+Execute: enrollment_script.py --id 01 --name -- Durian Lord
+"""
+import argparse
+import os
+import logging
+import requests
+
+import cv2
+import glob
+
+logging.basicConfig(level=logging.info(), format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger("enrollment")
+BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:8080")
+
+number_photos = 9
+
+def camera_enrollment(employee_id: int, name: str, number_of_photos = number_photos):
+    """
+    open the camera and take 'number_photos'.
+    User will get the instruction of taking photos
+    :param employee_id: id of the employee
+    :param name: name of the employee
+    :param number_of_photos: number of photos to take
+    :return:
+    """
+    logger.info(f"Start enrollment for: {name} with id = {employee_id} ...")
+    logger.info(f"It will take {number_of_photos} photos.")
+    logger.info(f"Please press SPACE for each photo, DELETE for cancel.")
+
+    camera = cv2.VideoCapture(0)
+    if not camera.isOpened():
+        logger.error("Camera not found.")
+        return False
+
+    photos = []
+    instructions = [
+        "Look directly in the camera",
+        "Look slightly to the left",
+        "Look slightly to the right",
+        "Look slightly to the top",
+        "Look slightly to the bottom",
+        "Smile",
+        "Look neutrally",
+        "Smile again",
+        "The last one, please look directly in the camera again",
+    ]
+
+    photo_nr = 0
+    while photo_nr < number_of_photos:
+        ok, frame = camera.read()
+        if not ok:
+            break
+
+        # show the instruction
+        instruction = (instructions[photo_nr] if photo_nr < len(instructions)
+                       else "Please press SPACE to take photos")
+
+        # write text into photo
+        h, w = frame.shape[:2]
+        cv2.putText(frame, f"photo {photo_nr+1}/{number_of_photos}",
+                    (10, 35), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        cv2.putText(frame, instruction,
+                    (10, h-20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 0), 2)
+        cv2.putText(frame, "SPACE = Photo --- DEL = Cancel",
+                    (10, h-50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), 1)
+        cv2.imshow(f"Enrollment of {name}", frame)
+        taste = cv2.waitKey(1) & 0xFF
+
+
+        if taste == 3014656: #DEL   # taste == ord('del')
+            logger.info(f"Enrollment cancelled!")
+            break
+        elif taste == 32: #SPACE
+            photos.append(frame.copy())
+            photo_nr += 1
+            logger.info(f"Photo {photo_nr+1}/{number_of_photos} taken")
+
+            # flash briefly as confirmation
+            cv2.putText(frame, "Photo taken sucessfully!",
+                        (w//2-100, h//2), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 200, 0), 2)
+            cv2.imshow(f"Enrollment of {name}", frame)
+            cv2.waitKey(500)
+
+    camera.release()
+    cv2.destroyAllWindows()
+
+    if not photos:
+        logger.error("No photos were taken.")
+        return False
+
+    # send photos to backend
+    logger.info(f"Sending {len(photos)} photos to backend ...")
+    return send_photos(employee_id, photos, name)
+
+def send_photos(employee_id: int, photos: list, name: str) -> bool:
+    """
+    send alls photos to /enrollment endpoint
+    :param employee_id: 
+    :param photos: 
+    :param name: 
+    :return: 
+    """
+    try:
+        files = []
+        for i, p in enumerate(photos):
+            # OpenCV -> JPEG Bytes
+            ok, buffer = cv2.imencode(".jpg", p, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+            if not ok:
+                continue
+            files.append(("photos", (f"photo_{i:02d}.jpg", buffer.tobytes(), "image/jpeg")))
+            
+        url = f"{BACKEND_URL}/enrollment/{employee_id}"
+        response = requests.post(url, files=files, timeout=60)
+        
+        if response.status_code == 200:
+            datas = response.json()
+            mes = datas["message"]
+            handled = datas["handling_images"]
+            missing = datas["missing_images"]
+            logger.info(f"Successful!")
+            logger.info(f"{mes}")
+            logger.info(f"Handled images: {handled}")
+            if datas.get("missing_images"):
+                logger.warning(f"Error: {missing}")
+            return True
+        else:
+            logger.error(f"Error: {response.status_code}")
+            logger.error(f"{response.text}")
+
+    except requests.exceptions.ConnectionError:
+        logger.error(f"Backend not available. {BACKEND_URL}")
+        logger.error("Check whether system was started correctly? (docker-compose up)")
+        return False
+
+def folder_enrollment (employee_id: int, name: str, folder_path: str) -> bool:
+    """
+    alternative: load photos from folder instead of using webcam
+    :param employee_id:
+    :param name:
+    :param folder_path:
+    :return:
+    """
+    photo_paths = (glob.glob(f"{folder_path}/*.jpg") +
+                  glob.glob(f"{folder_path}/*.png") +
+                  glob.glob(f"{folder_path}/*.jpeg"))
+
+    if not photo_paths:
+        logger.error(f"No photos in folder {folder_path} found.")
+        return False
+
+    logger.info(f"Found {len(photo_paths)} photos in folder {folder_path}")
+    photos = []
+    for path in photo_paths:
+        img = cv2.imread(path)
+        if img is not None:
+            photos.append(img)
+
+    return send_photos(employee_id, photos, name)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Enrollment for Employees of SleepyDurian")
+    parser.add_argument("__id", type=int, required=True, help="Employee-ID")
+    parser.add_argument("__name", type=str, required=True, help="Employee-Name")
+    parser.add_argument("__folder", type=str, required=True, help="Folder with photos (no webcam)")
+    parser.add_argument("__photos", type=str, default=None, help="Photos to send to backend")
+
+
+            
+            
